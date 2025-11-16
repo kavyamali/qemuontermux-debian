008caf5cd07deb0a1a0ff83808d5c7fa1a2f482d
Kavya Mali <kavyamali2904@gmail.com>
2025-11-16 17:45:16 +0530

    Update installdebian_raw.sh

--- installdebian_raw.sh
+++ installdebian_raw.sh
@@ -1,8 +1,9 @@
 pkg update -y
 pkg upgrade -y
-pkg install -y qemu-system-aarch64 openssh wget
+pkg install -y qemu-system-aarch64 openssh wget qemu-utils ovmf
 wget https://github.com/kavyamali/qemuontermux-debian/releases/download/qcow2/debian.qcow2.part.aa
 wget https://github.com/kavyamali/qemuontermux-debian/releases/download/qcow2/debian.qcow2.part.ab
 wget https://github.com/kavyamali/qemuontermux-debian/releases/download/qcow2/debian.qcow2.part.ac
 cat debian.qcow2.part.* > debian.qcow2
 rm debian.qcow2.part.*
+cp /data/data/com.termux/files/usr/share/edk2/aarch64/QEMU_EFI.fd .
